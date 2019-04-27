f02fb4033caedd4b3e90e324561b093099568725
draganstojanov <dragan.stojanov@gmail.com>
2019-04-28 00:52:58 +0200

    Cordova

--- Memory-CORDOVA/www/memory.js
+++ Memory-CORDOVA/www/memory.js
@@ -4,49 +4,99 @@
 var itemsArray = new Array();
 var item = {};
 var FIELDS_NUMBER = 16;
-//var randItem = [0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3, 4, 5, 6, 7];
 var randArray = new Array();
+var result = {};
+
+
 function memoryStart() {
     newGame(false);
 
 }
+
+
+
+function showTable(clickable) {
+
+   $("#current_moves").text(result.curentMoves);
+    $("#current_time").text(result.currentTime);
+    
+    for (var i = 0; i < FIELDS_NUMBER; i++) {
+        var itm = document.getElementById('item_' + i);
+        var img = document.getElementById('img_' + i);
+
+        item = itemsArray[i];
+        if (item.solved || item.open) {
+            $("#item_" + i).html(item.value);
+            if (item.solved) {
+                $("#item_" + i).css('background-color', 'red');
+            }
+
+        } else {
+            $("#img_" + i).attr("src", "andraganoid.jpg");
+        }
+    }
+
+    if (clickable) {
+        $("#new_game_btn").hide();
+        $(".table_item").on("click", function () {
+
+            var tid = this.id;
+            tid = tid.substring(5);
+            tid = tid.replace("_", "");
+            if (!itemsArray[tid].open || !itemsArray[tid].solves)
+            {
+                $(".table_item").off("click");
+                itemClicked(tid);
+            }
+        });
+    }
+}
+
+function itemClicked(id) {
+    itemsArray[id].open = true;
+
+    showTable(true);
+
+
+}
+
 function newGame(clickable) {
-    itemsArray = [];
-//    randItem.sort(() => Math.random() - 0.5);
-//    randItem.sort(() => Math.random() - 0.5);
-//    randItem.sort(() => Math.random() - 0.5);
     randArray = [];
     for (var i = 0; i < FIELDS_NUMBER / 2; i++) {
         randArray.push(i);
         randArray.push(i);
     }
- randArray.random();
+
+    randArray.sort(function (a, b) {
+        return 0.5 - Math.random();
+    });
+    randArray.sort(function (a, b) {
+        return 0.5 - Math.random();
+    });
+    randArray.sort(function (a, b) {
+        return 0.5 - Math.random();
+    });
+
+    itemsArray = [];
 
     for (var i = 0; i < FIELDS_NUMBER; i++) {
         item = {};
         item.value = randArray[i];
         item.index = i;
-        item.open = true;
+        item.open = false;
         item.solved = false;
         itemsArray.push(item);
     }
+    result = {};
+    result.curentMoves = 0;
+    result.currentTime = 0;
+    result.bestMoves = localStorage.bestMoves;
+    result.bestTime = localStorage.bestTime;
+    
+    alert(result.bestMoves)
+alert(result.bestTime)
+  $("#best_moves").text(result.bestMoves);
+   $("#best_time").text(result.bestTime);
+
     showTable(clickable);
-}
-
-
-function showTable(clickable) {
-    for (var i = 0; i < FIELDS_NUMBER; i++) {
-        var itm = document.getElementById('item_' + i);
-        var img = document.getElementById('img_' + i);
-        img.src = "andraganoid.jpg";
-        item = itemsArray[i];
-        if (item.solved || item.open) {
-            img.style.visibility = "hidden";
-            itm.innerHTML = item.value;
-
-        } else {
-            img.style.visibility = "visible";
-            itm.innerHTML = "";
-        }
-    }
 }
