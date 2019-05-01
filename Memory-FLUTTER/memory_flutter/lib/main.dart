40c94bccc74575f8023d56ca9def4b9a980e93ab
draganstojanov <dragan.stojanov@gmail.com>
2019-05-02 01:38:56 +0200

    Flutter

--- Memory-FLUTTER/memory_flutter/lib/main.dart
+++ Memory-FLUTTER/memory_flutter/lib/main.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
 
+List<Field> fieldsList;
+var bMoves;
+var bTime;
+var cMoves;
+var cTime;
+final int FIELDS_NUMBER = 16;
+final int FIELD_ROWS = 4;
+final int FIELD_COLS = 4;
+
 void main() {
   runApp(MemoryMain());
 }
@@ -26,13 +35,26 @@ class Memory extends StatefulWidget {
 }
 
 class _MemoryState extends State<Memory> {
-  var bMoves = 100;
-  var bTime = 1000 * 1000;
-  var cMoves = 0;
-  var cTime = 0;
-  final int FIELDS_NUMBER = 16;
-  final int FIELD_ROWS = 4;
-  final int FIELD_COLS = 4;
+  _MemoryState() {
+    _newGame();
+  }
+
+  void _newGame() {
+    fieldsList = List(FIELDS_NUMBER);
+    for (var i = 0; i < FIELDS_NUMBER / 2; i++) {
+      fieldsList[i] = (Field(i));
+      fieldsList[i + (FIELDS_NUMBER / 2).toInt()] = (Field(i));
+      print(i);
+    }
+    fieldsList.shuffle();
+    fieldsList.shuffle();
+    fieldsList.shuffle();
+
+    bMoves = 100;
+    bTime = 1000 * 1000;
+    cMoves = 0;
+    cTime = 0;
+  }
 
 //// Default placeholder text
 //  String textToShow = "I Like Flutter";
@@ -45,7 +67,6 @@ class _MemoryState extends State<Memory> {
 //  }
 //
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -53,38 +74,98 @@ class _MemoryState extends State<Memory> {
           title: Text("Memory-FLUTTER"),
         ),
         body: Column(children: <Widget>[
-          Row(
-            children: <Widget>[Text("Best moves: "), Text(bMoves.toString())],
-          ),
-          Row(
-            children: <Widget>[Text("Best time: "), Text(bTime.toString())],
-          ),
-          Expanded(
-            child: GridView.count(
-              crossAxisCount: FIELD_ROWS,
-              children: List.generate(FIELDS_NUMBER, (index) {
-                return IndexedStack(index: 1, children: <Widget>[
-                  Text(
-                    index.toString(),
-                    style: Theme.of(context).textTheme.headline,
-                  ),
-                  Image.asset(
-                    'assets/andraganoid.jpg',
-                    fit: BoxFit.fill,
-                  )
-                ]);
-              }),
-            ),
-          ),
-          Row(
-            children: <Widget>[
-              Text("Current moves: "),
-              Text(cMoves.toString())
-            ],
-          ),
-          Row(
-            children: <Widget>[Text("Current time: "), Text(cTime.toString())],
-          ),
+          _setRow("Best moves: ", bMoves),
+          _setRow("Best time: ", bTime ~/ 1000),
+          _setTable(),
+          _setRow("Current moves: ", cMoves),
+          _setRow("Currentt time: ", cTime ~/ 1000),
+          MaterialButton(
+            onPressed: _newGame,
+            color: Colors.green,
+            child: Text("New game"),
+          )
         ]));
+  }
+
+  Widget _setRow(txt, val) {
+    return Row(
+      children: <Widget>[_setText(txt), _setText(val.toString())],
+    );
+  }
+
+  Widget _setText(txt) {
+    return Padding(
+        padding: EdgeInsets.all(6),
+        child: Text(
+          txt,
+          style: TextStyle(fontSize: 20),
+        ));
+  }
+
+  Widget _setTable() {
+    return Expanded(
+        child: Column(
+      children: _getItemRows(),
+    ));
+  }
+
+  List<Widget> _getItemRows() {
+    List<Widget> itemRows = List(FIELD_ROWS);
+
+    for (var i = 0; i < FIELD_ROWS; i++) {
+      itemRows[i] = Row(children: _getItemCols(i));
+    }
+    return itemRows;
+  }
+
+
+//  child: IntrinsicHeight(
+//  child: Row(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+//  Expanded(
+
+  List<Widget> _getItemCols(row) {
+    List<Widget> itemCols = List(FIELD_COLS);
+
+    for (var j = 0; j < FIELD_COLS; j++) {
+      itemCols[j] = _getItem(row * FIELD_ROWS + j);
+    }
+    return itemCols;
+  }
+
+  Widget _getItem(indx) {
+//    return Text(
+//            indx.toString(),
+//            style: Theme.of(context).textTheme.headline,
+//          );
+
+    var itm = fieldsList[indx];
+    return Expanded(
+        child: Container(
+      child: IndexedStack(
+        index: itm.open ? 1 : 0,
+        //    index: 1,
+        children: <Widget>[
+          Image.asset(
+            'assets/andraganoid.jpg',
+          ),
+          Text(
+            itm.item.toString(),
+            style: Theme.of(context).textTheme.headline,
+          ),
+        ],
+      ),
+    ));
+  }
+}
+
+class Field {
+  int item;
+  bool open;
+  bool solved;
+
+  Field(int item) {
+    this.item = item;
+    this.open = false;
+    this.solved = false;
   }
 }
